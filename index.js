38104a735f850677ccbf36157dc096972e9c3c76
Dean Baberowski <deanbaberows@gmail.com>
2025-04-28 10:35:07 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+console.log(random);
+
+let num3 = 14; // Dividend
+let num4 = 5;  // Divisor
+let mod = num3 % num4; // Calculates remainder
+
+let max = Math.max(1, 2, 3, 4, 5, 20); // The maximum value from the given numbers
+console.log(max);
+console.log(mod);
